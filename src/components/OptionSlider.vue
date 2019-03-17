de72d14516ad3c9620ffda248a32fc6e573e2384
Enrico Marocco <enrico.marocco@telecomitalia.it>
2019-03-17 12:32:54 +0100

    Refactored options screen

---
+++ src/components/OptionSlider.vue
@@ -0,0 +1,85 @@
+<template>
+  <v-card flat>
+    <v-subheader class="subheading font-weight-medium text-uppercase">
+      {{title}}
+    </v-subheader>
+    <v-card-text>
+      <v-layout justify-space-between mb-3>
+        <v-flex text-xs-left>
+          <span class="subheading font-weight-light">
+            {{value}}
+          </span>
+          <span class="caption font-weight-light mr-1">
+            {{unit}}
+          </span>
+        </v-flex>
+        <v-flex text-xs-right>
+          <span class="subheading font-weight-light">
+            {{secondaryValue}}
+          </span>
+          <span class="caption font-weight-light mr-1">
+            {{secondaryUnit}}
+          </span>
+        </v-flex>
+      </v-layout>
+      <v-slider
+        :value="value" :min="min" :max="max" :step="step" always-dirty
+        @input="$emit('input', $event)">
+        <template v-slot:prepend>
+          <v-icon @click="$emit('input', value - 1)">
+            remove
+          </v-icon>
+        </template>
+        <template v-slot:append>
+          <v-icon  @click="$emit('input', value + 1)">
+            add
+          </v-icon>
+        </template>
+      </v-slider>
+    </v-card-text>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 0
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    unit: {
+      type: String
+    },
+    min: {
+      type: Number,
+      default: 0
+    },
+    max: {
+      type: Number,
+      default: 100
+    },
+    step: {
+      type: Number,
+      default: 1
+    },
+    secondaryValue: {
+      type: Number
+    },
+    secondaryUnit: {
+      type: String
+    }
+  },
+  data() {
+    return {
+
+    }
+  }
+
+}
+</script>
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,108 +1,31 @@
 <template>
   <v-container v-if="!started">
+
     <v-card
       class="mx-auto" max-width="600" flat>
-      <v-subheader class="subheading font-weight-medium text-uppercase">
-        Pace
-      </v-subheader>
-      <v-card-text>
-        <v-layout justify-space-between mb-3>
-          <v-flex text-xs-left>
-            <span class="subheading font-weight-light">
-              {{pace}}
-            </span>
-            <span class="caption font-weight-light mr-1">
-              seconds
-            </span>
-          </v-flex>
-          <v-flex text-xs-right>
-            <span class="subheading font-weight-light">
-              {{Math.round(60/pace)}}
-            </span>
-            <span class="caption font-weight-light mr-1">runs/minute</span>
-          </v-flex>
-        </v-layout>
-        <v-slider
-          v-model="pace" min="2" max="10" step="0.5" always-dirty>
-          <template v-slot:prepend>
-            <v-icon @click="pace--">
-              remove
-            </v-icon>
-          </template>
-          <template v-slot:append>
-            <v-icon @click="pace++">
-              add
-            </v-icon>
-          </template>
-        </v-slider>
-      </v-card-text>
+      <option-slider
+        v-model="pace" :min="2" :max="10" :step="0.5"
+        title="Pace" :unit="`second${pace == 1 ? '' : 's'}`"
+        :secondaryValue="Math.round(60 / pace)"
+        secondaryUnit="runs/minute"/>
 
-      <v-subheader class="subheading font-weight-medium text-uppercase">
-        Runs
-      </v-subheader>
-      <v-card-text>
-        <v-layout justify-space-between mb-3>
-          <v-flex text-xs-left>
-            <span class="subheading font-weight-light">
-              {{runs}}
-            </span>
-            <span class="caption font-weight-light mr-1">
-              run{{runs === 1 ? '' : 's'}}
-            </span>
-          </v-flex>
-          <v-flex text-xs-right>
-            <span class="subheading font-weight-light">
-              {{Math.round(pace * runs)}}
-            </span>
-            <span class="caption font-weight-light mr-1">
-              seconds
-            </span>
-          </v-flex>
-        </v-layout>
-        <v-slider
-          v-model="runs" min="1" max="50" step="1" always-dirty>
-          <template v-slot:prepend>
-            <v-icon @click="runs--">
-              remove
-            </v-icon>
-          </template>
-          <template v-slot:append>
-            <v-icon @click="runs++">
-              add
-            </v-icon>
-          </template>
-        </v-slider>
-      </v-card-text>
+      <option-slider
+        v-model="runs" :min="1" :max="50" :step="1"
+        title="Runs" :unit="`run${runs == 1 ? '' : 's'}`"
+        :secondaryValue="Math.round(pace * runs)"
+        secondaryUnit="seconds"/>
 
-      <v-subheader class="subheading font-weight-medium text-uppercase">
-        Countdown
-      </v-subheader>
-      <v-card-text>
-        <v-layout justify-space-between mb-3>
-          <v-flex text-xs-left>
-            <span class="subheading font-weight-light">
-              {{countdown}}
-            </span>
-            <span class="caption font-weight-light mr-1">
-              seconds
-            </span>
-          </v-flex>
-        </v-layout>
-        <v-slider
-          v-model="countdown" min="0" max="120" step="1" always-dirty>
-          <template v-slot:prepend>
-            <v-icon @click="countdown--">
-              remove
-            </v-icon>
+      <v-expansion-panel popout>
+        <v-expansion-panel-content>
+          <template v-slot:header>
+            More Options
           </template>
-          <template v-slot:append>
-            <v-icon @click="countdown++">
-              add
-            </v-icon>
-          </template>
-        </v-slider>
-      </v-card-text>
-      
+          <option-slider
+            v-model="countdown" :min="0" :max="120" :step="1"
+            title="Countdown" :unit="`second${countdown == 1 ? '' : 's'}`"/>
+        </v-expansion-panel-content>
+      </v-expansion-panel>
+
       <v-card-text>
         <v-btn
           block large color="blue" @click="start">
@@ -120,12 +43,14 @@
 <script>
 import NoSleep from 'nosleep.js'
 import Director from '../components/Director'
+import OptionSlider from '../components/OptionSlider'
 
 const noSleep = new NoSleep()
 
 export default {
   components: {
-    Director
+    Director,
+    OptionSlider
   },
   data() {
     return {
